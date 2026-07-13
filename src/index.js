0214704f21ac0c3bc774f8b8d4f30ec96250638c
Matt Porter <matt@diligentwealth.co.nz>
2026-07-13 16:12:37 +1200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import WealthGuardTool from './App';
 import LoginGate from './LoginGate';
-// ...
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <LoginGate>
-    <WealthGuardTool />
-  </LoginGate>
+  <React.StrictMode>
+    <LoginGate>
+      <WealthGuardTool />
+    </LoginGate>
+  </React.StrictMode>
 );
